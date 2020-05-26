5b564f1f5752496d20440c5b54dc318843a76dec
ilie_petrasco <petras.moil@gmail.com>
2020-05-27 00:16:12 +0300

    prod settings

--- backend/home/settings/dev.py
+++ backend/home/settings/dev.py
@@ -2,7 +2,7 @@
 
 from .base import *
 
-ALLOWED_HOSTS += ['127.0.0.1']
+ALLOWED_HOSTS += ['127.0.0.1', 'http://ec2-54-211-123-16.compute-1.amazonaws.com']
 DEBUG = True
 
 WSGI_APPLICATION = 'home.wsgi.dev.application'
