57d3bcd271d6a85bd99e13d6be3494fb58eb024b
ilie_petrasco <petras.moil@gmail.com>
2020-05-30 10:51:58 +0300

    prod settings

--- backend/home/settings/prod.py
+++ backend/home/settings/prod.py
@@ -7,21 +7,13 @@ ALLOWED_HOSTS += ['*']
 WSGI_APPLICATION = 'home.wsgi.prod.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'database-1',
-        'USER': 'postgres',
-        'PASSWORD': 'qweasdzxc',
-        'HOST': 'database-1.cocz6fyvolcf.us-east-1.rds.amazonaws.com',
-        'PORT': '5432',
-    }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'db_name',
-    #     'USER': 'db_user',
-    #     'PASSWORD': 'db_password',
-    #     'HOST': 'localhost',
-    #     'PORT': '',
+    #     'NAME': 'database-1',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'qweasdzxc',
+    #     'HOST': 'database-1.cocz6fyvolcf.us-east-1.rds.amazonaws.com',
+    #     'PORT': '5432',
     # }
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
